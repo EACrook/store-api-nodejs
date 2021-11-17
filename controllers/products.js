d4e6ee21286001ab8034c9f78790797fadb41171
Emily Crook <Emily.A.Crook96@gmail.com>
2021-11-16 18:19:22 -0600

    use select functionality to limit the amount of information shown

--- controllers/products.js
+++ controllers/products.js
@@ -3,31 +3,39 @@ const Product = require('../models/product');
 const getAllProductsStatic = async (req, res) => {
     // throw new Error('testing async errors');
     const search = 'ab';
-    const products = await Product.find({}).sort('-name price');
+    const products = await Product.find({}).select('name price');
     res.status(200).json({msg: products, nbHits: products.length });
 }
 
 const getAllProducts = async (req, res) => {
-    const {featured, company, name, sort} = req.query;
+    const {featured, company, name, sort, fields} = req.query;
     const queryObject = {}
-
+    // search by featured items
     if (featured) {
         queryObject.featured = featured === 'true'? true : false;
     }
+    // search by company
     if (company) {
         queryObject.company = company;
     }
+    // search by object name
     if (name) {
         queryObject.name = { $regex:name, $options: 'i' };
     }
     // console.log(queryObject);
     let result = Product.find(queryObject);
+    // sort the items by certain parameters
     if(sort){
         const sortList = sort.split(',').join(' ');
         result = result.sort(sortList);
         // products = products.sort()
     } else {
         result = result.sort('createdAt');
+    }
+    // only show selected fields in search
+    if(fields){
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
     }
     const products = await result
     res.status(200).json({ products, nbHits: products.length})
