3b4fc78d8e22bf3f1a7cfd8cc0c1ed6aad46f041
Emily Crook <Emily.A.Crook96@gmail.com>
2021-11-16 18:39:49 -0600

    set up limit parameter and skip parameter to allow for functionality on the amount of products shown per page

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 const getAllProductsStatic = async (req, res) => {
     // throw new Error('testing async errors');
     const search = 'ab';
-    const products = await Product.find({}).select('name price');
+    const products = await Product.find({}).sort('name').select('name price').limit(10).skip(10);
     res.status(200).json({msg: products, nbHits: products.length });
 }
 
@@ -37,8 +37,16 @@ const getAllProducts = async (req, res) => {
         const fieldsList = fields.split(',').join(' ');
         result = result.select(fieldsList);
     }
-    const products = await result
-    res.status(200).json({ products, nbHits: products.length})
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+    // 23 items
+    // we will effectively have 4 pages of 7 each except for the last page
+
+    const products = await result;
+    res.status(200).json({ products, nbHits: products.length});
 }
 
 module.exports = {
